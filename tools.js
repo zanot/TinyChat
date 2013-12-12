47a9aa3f0167f9d66898e56992948a1eb6d39678
shichon64 <shichon64@msn.com>
2013-12-12 14:20:59 +0100

    Lecture templates en async
    Lectures des fichiers templates en asynchrone (au lieu de synchrone :
    readFileSync) et compression code.

--- tools.js
+++ tools.js
@@ -1,8 +1,8 @@
+var fs = require('fs');
 
 /**
  * Fonctions utiles
  */
-
 module.exports = {
 	log: function (txt) {
 		console.log('[LOG] - ' + txt);
@@ -11,5 +11,13 @@ module.exports = {
 	getDate: function () {
 		var d = new Date();
 		return d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+	},
+
+	generate: function (tpl_path, callback) {
+		fs.readFile(tpl_path, 'utf8', function (error, tpl) {
+			if (error) { throw error; }
+
+			callback(tpl);
+		});
 	}
 };
